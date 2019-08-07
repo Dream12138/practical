7adeab2a7eaecc4dffa10bdad1cd698a3d75efba
zhangxiaolong <522196937@qq.com>
2019-08-07 14:35:17 +0800

    初始化

---
+++ springbootmvcweb002/src/main/java/com/neusoft/springboot/Springbootmvcweb002Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootmvcweb002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootmvcweb002Application.class, args);
+	}
+
+}
